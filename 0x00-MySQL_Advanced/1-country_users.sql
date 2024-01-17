11b4abf6f914702bdcf1aaa41b640cd7baab15a4
mahmoud-elbanna <melbanna964@gmail.com>
2024-01-17 22:36:38 +0000

    Fix Syntax

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,8 @@
 -- Script that creates a table users following these requirements:
 -- id, email, name, country(enumeration of US, CO and TN)
-ALTER TABLE users
-ADD COLUMN country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL;
-
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
